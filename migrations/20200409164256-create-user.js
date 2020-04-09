e7aa87acbe210b590002d37882bc5325d335f5c8
Basse <babacar.basse@esp,sn>
2020-04-09 22:14:13 +0000

    integrate uuid

--- migrations/20200409164256-create-user.js
+++ migrations/20200409164256-create-user.js
@@ -4,9 +4,9 @@ module.exports = {
     return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
+        type: Sequelize.UUID,
+        defaultValue: uuid()
       },
       phone: {
         type: Sequelize.STRING,
