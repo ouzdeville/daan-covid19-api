f916d5a24a793740ca0fc29ded09f0db4b8f0afe
Basse <babacar.basse@esp,sn>
2020-04-10 17:31:31 +0000

    register location kinesis

--- controller/location.js
+++ controller/location.js
@@ -1,17 +1,25 @@
-const { Location } = require('./../models');
+const { Location, User } = require('./../models');
+const { awsClients } = require('./../utils');
 
 module.exports = {
-  registerLocation(req, res) {
-    const location = req.body;
-    Location.create(location)
-      .then((_location) => {
-        res.status(201).send({
-          success: true,
-          message: 'Successfully created.',
-          _location,
-        });
-      })
-      .catch((error) => res.status(400).send(error));;
+  async registerLocation(req, res) {
+    try {
+      const { userID } = req;
+      const payload = {
+        id: userID,
+        imei: req.body.imei,
+        timestamp: req.body.timestamp,
+        lng: req.body.lng,
+        lat: req.body.lat,
+      };
+      await awsClients.writeToKinesis(payload);
+      res.status(201).send({
+        success: true,
+        message: 'Successfully registered.',
+      });
+    } catch (error) {
+      res.status(401).send({ error });
+    }
   },
 
   getUserLocations(req, res) {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,24 @@
 const { jwt } = require('./../providers');
+const { User } = require('./../models');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token);
     req.phone = decodedToken.phone;
+    const exist = await User.findAll({
+      where: {
+        phone: decodedToken.phone,
+      },
+    });
+    if (!exist || !exist.length) {
+      res.status(401).send({error: 'token invalid.'});
+      return;
+    }
+    req.userID = exist[0].id;
     next();
-  } catch {
+  } catch(error) {
+    console.log(error);
     res.status(401).send({error: 'token invalid'});
   }
 };
