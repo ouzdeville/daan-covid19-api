be7016ec74ef9e89a8351981086881400e560e32
Basse <babacar.basse@esp,sn>
2020-04-10 14:16:47 +0000

    catch error

--- controller/user.js
+++ controller/user.js
@@ -3,50 +3,58 @@ const { otpProvider, jwt } = require('./../providers');
 
 module.exports = {
   async create(req, res) {
-    if (req.headers.issuer !== process.env.OAUTH_SECRET) {
-      res.status(401).send({
-        message: 'invalid issuer.',
+    try {
+      if (req.headers.issuer !== process.env.OAUTH_SECRET) {
+        res.status(401).send({
+          message: 'invalid issuer.',
+        });
+        return;
+      }
+      const otp = await otpProvider.generateOTP(req.body.phone);
+      const token = jwt.sign({ phone: req.body.phone });
+      const exist = await User.findAll({
+        where: {
+          phone: req.body.phone,
+        },
       });
-      return;
-    }
-    const otp = await otpProvider.generateOTP(req.body.phone);
-    const token = jwt.sign({ phone: req.body.phone });
-    const exist = await User.findAll({
-      where: {
-        phone: req.body.phone,
-      },
-    });
-    if (exist && !exist.length) {
-      User.create({
-        active: 'pending',
-        phone: req.body.phone,
-      })
-        .then((user) => {
-          res.status(201).send({
-            success: true,
-            message: 'Successfully created.',
-            token,
-          });
+      if (exist && !exist.length) {
+        User.create({
+          active: 'pending',
+          phone: req.body.phone,
         })
-        .catch((error) => res.status(400).send(error));
-      return;
+          .then((user) => {
+            res.status(201).send({
+              success: true,
+              message: 'Successfully created.',
+              token,
+            });
+          })
+          .catch((error) => res.status(400).send(error));
+        return;
+      }
+      res.status(201).send({
+        success: true,
+        message: 'Successfully created.',
+        token,
+      });
+    } catch (error) {
+      res.status(500).send(error);
     }
-    res.status(201).send({
-      success: true,
-      message: 'Successfully created.',
-      token,
-    });
   },
 
   async verifyCode(req, res) {
-    const verification = await otpProvider.verifyOtp({
-      code: req.body.code,
-      phone: req.phone,
-    });
-    if (verification) {
-      res.send({ success: true, message: 'Successfully verified.' });
+    try {
+      const verification = await otpProvider.verifyOtp({
+        code: req.body.code,
+        phone: req.phone,
+      });
+      if (verification) {
+        res.send({ success: true, message: 'Successfully verified.' });
+      }
+      res.status(401).send({ message: 'verification error' });
+    } catch(error) {
+      res.status(500).send(error)
     }
-    res.status(401).send({ message: 'verification error' });
   },
 
   get(req, res) {
