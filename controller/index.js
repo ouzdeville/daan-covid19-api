daedc114cd9a9b0847aa9e76c10e600d0604d169
darousalam <serigneasse.gueye@uadb.edu.sn>
2020-04-18 19:23:22 +0200

    add route for barrier gesture, green number and symtom

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,7 @@
 module.exports = {
   UserController: require('./user'),
   LocationController: require('./location'),
+  BarrierGestureController: require('./barrier-gesture'),
+  GreenNumberController: require('./green-number'),
+  SymptomController: require('./symptom'),
 }
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const { UserController, LocationController } = require('./../controller');
+const { UserController, LocationController, BarrierGestureController, GreenNumberController, SymptomController } = require('./../controller');
 const { auth } = require('./../middlewares');
 
 module.exports = (app) => {
@@ -9,4 +9,19 @@ module.exports = (app) => {
   app.post('/user/verify_code', auth, UserController.verifyCode);
   app.post('/location', auth, LocationController.registerLocation);
   app.get('/location/:idUser', auth, LocationController.getUserLocations);
+
+  // Route Barrier Gesture
+  app.post('/barrier-gesture', auth, BarrierGestureController.create);
+  app.get('/barrier-gesture/:id', auth, BarrierGestureController.get);
+  app.get('/barrier-gestures', auth, BarrierGestureController.getAllBarrierGesture);
+
+  // Route Green Number
+  app.post('/green-number', auth, GreenNumberController.create);
+  app.get('/green-number/:id', auth, GreenNumberController.get);
+  app.get('/green-numbers', auth, GreenNumberController.getAllGreenNumber);
+
+  // Route Symptom
+  app.post('/symptom', auth, SymptomController.create);
+  app.get('/symptom/:id', auth, SymptomController.get);
+  app.get('/symptoms', auth, SymptomController.getAllSymptom);
 };
