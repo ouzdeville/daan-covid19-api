59e047600c9e71a52fc2b49ba91b032a70d08290
Cheikh Sidya CAMARA <sidya.camara@gmail.com>
2020-04-19 17:49:05 +0000

    check if the report of the specified day exist before to create it elsewise we update it.

--- controller/daily-report.js
+++ controller/daily-report.js
@@ -1,7 +1,8 @@
 const {DailyReport} = require('./../models');
 
 module.exports = {
-    create(req, res) {
+    async create(req, res) {
+        reportDate = req.body.reportDate;
         const data = {
             reportDate: req.body.reportDate,
             numberOfTest: req.body.numberOfTest,
@@ -19,15 +20,34 @@ module.exports = {
             totalCases: req.body.totalCases
         };
 
-        DailyReport.create(data)
-            .then((dailyReport) => {
-                res.status(201).send({
-                    success: true,
-                    message: 'Successfully created.',
-                    dailyReport,
-                });
-            })
-            .catch((error) => res.status(400).send(error));
+        // check if the report for the date (reportDate) already exist
+        const dailyReport = await DailyReport.findOne({ where: { reportDate: reportDate } });
+
+        if (dailyReport === null) {
+            // if it doesn't exist we create it
+            DailyReport.create(data)
+                .then((dailyReport) => {
+                    res.status(201).send({
+                        success: true,
+                        message: 'Successfully created.',
+                        dailyReport,
+                    });
+                })
+                .catch((error) => res.status(400).send(error));
+        } else {
+            // if it exist we just update it
+            DailyReport.update(
+                data,
+                { where: { reportDate: reportDate } }
+            )
+                .then((dailyReport) => {
+                    res.status(201).send({
+                        success: true,
+                        message: 'Successfully updated.',
+                    });
+                })
+                .catch((error) => res.status(400).send(error));
+        }
     },
 
     getAll(req, res) {
