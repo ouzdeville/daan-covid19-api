650aa73f191e3e9b19e683c993e967fb45637fdf
ousmane <ouzdeville@gmail.com>
2020-04-22 16:50:28 +0000

    RiskFactor seed integration

---
+++ seeders/20200422154521-riskfactor.js
@@ -0,0 +1,80 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('RiskFactor', [{
+      id:1,
+      name: "Fumeur",
+      description: "",
+      type:"1",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:2,
+      name: "Consommation d'alcool",
+      description: "",
+      type:"1",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:3,
+      name: "Diabétique",
+      description: "",
+      type:"2",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:4,
+      name: "Hypertension artérielle",
+      description: "",
+      type:"2",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:5,
+      name: "Asthmatique",
+      description: "",
+      type:"2",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:6,
+      name: "",
+      description: "Maladie Cardiaque",
+      type:"2",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:7,
+      name: "Obésité",
+      description: "",
+      type:"2",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:8,
+      name: "Contact avec un malade",
+      description: "",
+      type:"3",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      id:9,
+      name: "Retour de voyage",
+      description: "",
+      type:"3",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
