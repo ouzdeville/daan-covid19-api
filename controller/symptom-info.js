6e372328494b7659f4b9546d7c24b94a1e29609d
sarrside <side.sarr@uadb.edu.sn>
2020-04-23 14:08:24 +0000

    Documentation of SymptomInfo

--- controller/symptom-info.js
+++ controller/symptom-info.js
@@ -1,6 +1,29 @@
 const { Symptom } = require('../models');
 
 module.exports = {
+
+
+  /**
+     * @api {post} /symptom Add Symptom
+     * @apiName Create
+     * @apiGroup SymptomInfo
+     *
+     * @apiParam {String} name id of Location
+     * @apiParam {Boolean} major is or not a major Symptom
+     * @apiParam {Text} description description of Symptom
+     * @apiParam {String} img image of SymtomInfo
+     * 
+     *
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 201 Created
+     *     {
+     *         "success" : true,
+     *         "code"    : 1,
+     *          "message": "Successfully created.",
+     *          "symptom": "newsymptom"
+     *     }
+     */
     create: function (req, res) {
       var name = req.body.name;
       var major = req.body.major;
@@ -18,7 +41,7 @@ module.exports = {
           img: img
         })
         .then(function(newsymptom){
-            return res.status(200).send({
+            return res.status(201).send({
               success: true,
               code:1,
               message: 'Successfully created.',
@@ -31,13 +54,45 @@ module.exports = {
           });
         });
     },
+
+    /**
+     * @api {get} /symptom/:id Get SymptomInfo by symptom id
+     * @apiName Get
+     * @apiGroup SymptomInfo
+     *
+     * @apiParam {Number} id id of the SymptomInfo
+     *
+     * 
+     * @apiSuccess (Success 200) {Number}  id            id of SymptomInfo
+     * @apiSuccess (Success 200) {String}  title         title of SymptomInfo
+     * @apiSuccess (Success 200) {Text}    content       content of SymptomInfo
+     * @apiSuccess (Success 200) {Boolean} major         is or not major Symptom
+     * @apiSuccess (Success 200) {Date}    createdAt     creation's date of Symptom
+     * @apiSuccess (Success 200) {DATE}    updatedAt     date of last update of Symptom
+     * 
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *          "symptom":
+     *              {
+     *                 "id": 1,
+     *                 "title": "Mal de tête",
+     *                 "content": "",
+     *                 "major": true,
+     *                 "img": "",
+     *                 "createdAt": "2020-04-22T21:18:48.066Z",
+     *                 "updatedAt": "2020-04-22T21:18:48.066Z"
+     *           }
+     *     }
+     */
     get: function (req, res) {
         var symptomId = req.params.id;
         Symptom.findOne({
           where: {id:symptomId}
         }).then(function(symptom){
           if(symptom) {
-            return res.status(201).send(symptom);
+            return res.status(200).send(symptom);
           } 
           else {
             res.status(404).send({
@@ -51,6 +106,45 @@ module.exports = {
           });
         });
     },
+
+
+    /**
+     * @api {get} /symptoms get all SymptomInfo
+     * @apiSuccess {Object[]} symptoms            List of SymptomInfo.
+     * @apiSuccess {Number}   symptom.id          id SymptomInfo.
+     * @apiSuccess {String}   symptom.title       title of SymptomInfo.
+     * @apiSuccess {Text}   symptom.content     content of SypmtomInfo.
+     * @apiSuccess {Boolean}   symptom.major       major of SymptomInfo.
+     * @apiSuccess {String}   symptom.img         image of SymptomInfo.
+     * @apiSuccess {Date}   symptom.createdAt   creation's date of SymptomInfo.
+     * @apiSuccess {Date}   symptom.updatedAt   last update's date of SymptomInfo.
+     * 
+     * 
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *        "somptoms": [
+     *          {
+     *             "id": 1,
+     *             "title": "Mal de tête",
+     *             "content": "",
+     *             "major": true,
+     *             "img": "",
+     *             "createdAt": "2020-04-22T21:18:48.066Z",
+     *             "updatedAt": "2020-04-22T21:18:48.066Z"
+     *           },
+     *           {
+     *               "id": 2,
+     *               "title": "Toux",
+     *               "content": "",
+     *               "major": true,
+     *               "img": "",
+     *               "createdAt": "2020-04-22T21:18:48.066Z",
+     *               "updatedAt": "2020-04-22T21:18:48.066Z"
+     *          },
+     *        ]
+     *     }
+     */
     getAllSymptom: function (req, res) {
       var fields = req.query.fields;
       var limit = parseInt(req.query.limit);
