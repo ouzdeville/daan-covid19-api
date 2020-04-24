552f2b687d0f71f64b2978136043ea4536f16b2b
sarrside <side.sarr@uadb.edu.sn>
2020-04-24 23:31:15 +0000

    Documentation of Symptom module

--- controller/symptom.js
+++ controller/symptom.js
@@ -1,47 +1,102 @@
-const { Symptom } = require('./../models');
+const { Symptom } = require('../models');
 
 module.exports = {
+
+
+  /**
+     * @api {post} /symptom Add Symptom
+     * @apiName Create
+     * @apiGroup Symptom
+     *
+     * @apiParam {String} name name of Symptom
+     * @apiParam {Boolean} major is or not a major Symptom
+     * @apiParam {Text} description description of Symptom
+     * @apiParam {String} img image of SymtomInfo
+     * 
+     *
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 201 Created
+     *     {
+     *         "success" : true,
+     *         "code"    : 1,
+     *          "message": "Successfully created.",
+     *          "symptom": "newsymptom"
+     *     }
+     */
     create: function (req, res) {
-        var name = req.body.name;
-        var major = req.body.major;
-        var img = req.body.img;
-        var description = req.body.description;
+      var name = req.body.name;
+      var major = req.body.major;
+      var img = req.body.img;
+      var description = req.body.description;
 
         if (name == null) {
             return res.status(400).send({ message: 'missing parameters' });
         }
 
-        var newSymtom = Symptom.create({
-            name: name,
-            description:description,
-            major: major,
-            img: img
+        var newsymptom = Symptom.create({
+          name: name,
+          description:description,
+          major: major,
+          img: img
         })
-        .then(function(newSymtom){
+        .then(function(newsymptom){
             return res.status(201).send({
               success: true,
               code:1,
               message: 'Successfully created.',
-              symtom: newSymtom
+              symptom: newsymptom
             });
         })
         .catch(function(err){
           return res.status(400).send({
-            error: 'Cannot add symtom'
+            error: 'Cannot add symptom'
           });
         });
     },
+
+    /**
+     * @api {get} /symptom/:id Get Symptom by symptom id
+     * @apiName Get
+     * @apiGroup Symptom
+     *
+     * @apiParam {Number} id id of the Symptom
+     *
+     * 
+     * @apiSuccess (Success 200) {Number}  id            id of Symptom
+     * @apiSuccess (Success 200) {String}  title         title of Symptom
+     * @apiSuccess (Success 200) {Text}    content       content of Symptom
+     * @apiSuccess (Success 200) {Boolean} major         is or not major Symptom
+     * @apiSuccess (Success 200) {Date}    createdAt     creation's date of Symptom
+     * @apiSuccess (Success 200) {DATE}    updatedAt     date of last update of Symptom
+     * 
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *          "symptom":
+     *              {
+     *                 "id": 1,
+     *                 "title": "Mal de tête",
+     *                 "content": "",
+     *                 "major": true,
+     *                 "img": "",
+     *                 "createdAt": "2020-04-22T21:18:48.066Z",
+     *                 "updatedAt": "2020-04-22T21:18:48.066Z"
+     *           }
+     *     }
+     */
     get: function (req, res) {
         var symptomId = req.params.id;
         Symptom.findOne({
           where: {id:symptomId}
-        }).then(function(symtom){
-          if(symtom) {
-            return res.status(200).send(symtom);
+        }).then(function(symptom){
+          if(symptom) {
+            return res.status(200).send(symptom);
           } 
           else {
             res.status(404).send({
-              error: 'Cannot add symtom'
+              error: 'Cannot add symptom'
             });
           }
         }).catch(function(err){
@@ -51,6 +106,47 @@ module.exports = {
           });
         });
     },
+
+
+    /**
+     * @api {get} /symptoms get all Symptom
+     * @apiName getAllSymptom
+     * @apiGroup Symptom
+     * @apiSuccess {Object[]} symptoms            List of Symptom.
+     * @apiSuccess {Number}   symptom.id          id Symptom.
+     * @apiSuccess {String}   symptom.title       title of Symptom.
+     * @apiSuccess {Text}   symptom.content     content of Symptom.
+     * @apiSuccess {Boolean}   symptom.major       major of Symptom.
+     * @apiSuccess {String}   symptom.img         image of Symptom.
+     * @apiSuccess {Date}   symptom.createdAt   creation's date of Symptom.
+     * @apiSuccess {Date}   symptom.updatedAt   last update's date of Symptom.
+     * 
+     * 
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *        "somptoms": [
+     *          {
+     *             "id": 1,
+     *             "title": "Mal de tête",
+     *             "content": "",
+     *             "major": true,
+     *             "img": "",
+     *             "createdAt": "2020-04-22T21:18:48.066Z",
+     *             "updatedAt": "2020-04-22T21:18:48.066Z"
+     *           },
+     *           {
+     *               "id": 2,
+     *               "title": "Toux",
+     *               "content": "",
+     *               "major": true,
+     *               "img": "",
+     *               "createdAt": "2020-04-22T21:18:48.066Z",
+     *               "updatedAt": "2020-04-22T21:18:48.066Z"
+     *          },
+     *        ]
+     *     }
+     */
     getAllSymptom: function (req, res) {
       var fields = req.query.fields;
       var limit = parseInt(req.query.limit);
@@ -62,9 +158,9 @@ module.exports = {
         attributes: (fields !=='*' && fields != null) ? fields.split(','): null,
         limit: (!isNaN(limit)) ? limit : null,
         offset: (!isNaN(offset)) ? offset : null
-      }).then(function(symptoms) {
+      }).then(function(symptoms) { 
         if(symptoms) {
-          return res.status(200).send(symtoms);
+          return res.status(200).send(symptoms);
         }
         else {
           res.status(404).send({
