2ad004409e07d25741a862a1f3f69c641a4ce654
Cheikh Sidya CAMARA <sidya.camara@gmail.com>
2020-04-26 20:31:27 +0000

    update test integration users

--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -7,14 +7,18 @@ describe('Users Endpoints', () => {
   it('should list users', async (done) => {
     await User.create({
       active: 'pending',
+      phone: '771111111',
+    })
+    await User.create({
+      active: 'pending',
       phone: '775555555',
     })
     jest.mock('../../middlewares', () => ({ auth: (_req, _res, next) => next() }))
     app = require('../../app');
     const res = await request(app).get('/users')
     expect(res.statusCode).toEqual(200)
-    expect(res.body.length).toEqual(1)
-    expect(res.body[0].phone).toEqual('775555555')
+    expect(res.body.length).toEqual(2)
+    expect(res.body[1].phone).toEqual('775555555')
     done()
   })
 })
