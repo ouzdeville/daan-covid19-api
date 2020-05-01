27e3eedcc6a13741e6fd9740fc910e6d58794139
Cheikh Sidya CAMARA <sidya.camara@gmail.com>
2020-05-01 06:59:32 +0000

    debug

--- controller/push-notification.js
+++ controller/push-notification.js
@@ -6,7 +6,7 @@ const TOPIC = 'daily-stats';
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "project_id": process.env.FIREBASE_PROJECT_ID,
   }),
