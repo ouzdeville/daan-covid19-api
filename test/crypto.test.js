0ac5fabe3798927c0d7091974e97ae0fee7346fc
ousmane <ouzdeville@gmail.com>
2020-05-02 23:18:22 +0000

    Cryto integration

--- test/crypto.test.js
+++ test/crypto.test.js
@@ -1,11 +1,23 @@
 const {cryptoUtil} = require('../utils');
 
-describe("getSymmetricKey(password)", () => {
-    it("should return random string", () => {
+describe("Crypto Tools", () => {
+    it("Crypto Tools should work", () => {
         //Another way to test a boolean
         key=cryptoUtil.getSymmetricKey("mot de passe");
-        msg= new Buffer("YEP DaanCovid")
+        console.log(key);
+        msg= "YEP DaanCovid";
         ciphertext=cryptoUtil.Encrypt(msg,key);
-        expect(cryptoUtil.Decrypt(ciphertext,key).toString('utf8')===msg.toString('utf8')).toBe(true);
+        console.log(ciphertext);
+        cleartext=cryptoUtil.Decrypt(ciphertext,key);
+        expect(cleartext===msg).toBe(true);
+
+        id="dc76809c-23b2-4e0d-91ce-14aa46aeb5b4";
+        SID=cryptoUtil.getSID(id,"mot de passe");
+        newId=cryptoUtil.getIdFromSID(SID,"mot de passe");
+        console.log(newId);
+        console.log(typeof id);
+        console.log(typeof newId);
+        expect(id===newId).toBe(true);
+
     });
 });
--- utils/crypto/crypto-util.js
+++ utils/crypto/crypto-util.js
@@ -8,39 +8,82 @@ module.exports = {
     IV: "qKrtJS287EKQfACW",
     CLEAR_ENCODE: "utf8",
     ENCRYPT_ENCODE: "binary",
+    MOD_LENGTH=2048,
+    KEY_FORMAT="pem",
+    PRINT_FORMAT='base64',
     getSymmetricKey(password) {
         const key = crypto.scryptSync(password, module.exports.SALT, module.exports.KEY_SIZE / 8);
 
-        return key;
+        return key.toString(module.exports.PRINT_FORMAT);
     },
 
-    Encrypt(clearText, key) {
+    Encrypt(clearText, skey) {
         try {
+            key=new Buffer(skey, module.exports.PRINT_FORMAT);
             const cipher = crypto.createCipheriv(module.exports.ALGO_SYM, key, module.exports.IV);
             return Buffer.concat([
-                cipher.update(clearText),
+                cipher.update(new Buffer(clearText)),
                 cipher.final()
-            ]);
+            ]).toString(module.exports.PRINT_FORMAT);
         } catch (error) {
             console.error(error);
         };
     },
 
-    Decrypt(encryptdata, key) {
+    Decrypt(encryptdata, skey) {
         try {
+            key=new Buffer(skey, module.exports.PRINT_FORMAT);
             const decipher = crypto.createDecipheriv(module.exports.ALGO_SYM, key, module.exports.IV);
             return Buffer.concat([
-                decipher.update(encryptdata),
+                decipher.update(new Buffer(encryptdata, module.exports.PRINT_FORMAT)),
                 decipher.final()
-            ]);
+            ]).toString(module.exports.CLEAR_ENCODE);
         } catch (error) {
             console.error(error);
         };
     },
 
     getSID(id, password) {
-        key = getSymmetricKey(password);
-        SID = Encrypt(id, key)
+        key = module.exports.getSymmetricKey(password);
+        SID = module.exports.Encrypt(id, key)
         return SID;
     },
+
+    getIdFromSID(SID, password) {
+        key = module.exports.getSymmetricKey(password);
+        id = module.exports.Decrypt(SID, key)
+        return id;
+    },
+    generateKeyPair(password) {
+
+        const keyPair = crypto.generateKeyPairSync('rsa', {
+            modulusLength: module.exports.MOD_LENGTH,
+            publicKeyEncoding: {
+                type: 'spki',
+                format: module.exports.KEY_FORMAT
+            },
+            privateKeyEncoding: {
+                type: 'pkcs8',
+                format: module.exports.KEY_FORMAT,
+                cipher: module.exports.ALGO_SYM,
+                passphrase: password
+            }
+        });
+        // Creating public and private key file  
+        //fs.writeFileSync("public_key", keyPair.publicKey); 
+        //fs.writeFileSync("private_key", keyPair.privateKey); 
+        return keyPair;
+    },
+    RSAencrypt(plaintext, publicKey) {
+        //const publicKey = fs.readFileSync(publicKeyFile, "utf8");
+        const encrypted = crypto.publicEncrypt(
+            publicKey, Buffer.from(plaintext));
+        return encrypted.toString(module.exports.PRINT_FORMAT);
+    },
+    RSAdecrypt(ciphertext, privateKey) {
+        //const privateKey = fs.readFileSync(privateKeyFile, "utf8");
+        const decrypted = crypto.privateDecrypt(
+            privateKey, Buffer.from(ciphertext, module.exports.PRINT_FORMAT));
+        return decrypted.toString(module.exports.CLEAR_ENCODE);
+    }
 }
