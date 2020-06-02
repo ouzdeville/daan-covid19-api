77e813f58d1e1b28e7baaa8aed5b561cf572bf42
Cheikh Sidya CAMARA <sidya.camara@gmail.com>
2020-06-02 18:06:10 +0000

    test push contacts

--- controller/user.js
+++ controller/user.js
@@ -248,7 +248,17 @@ module.exports = {
      *     }
      */
     async signaler(req, res) {
-        let {debutincubation, finincubation, idUser, sendNotification} = req.body
+        let {debutincubation, finincubation, idUser, sendNotification} = req.body;
+
+        await elasticClient.getUserContactsNew(id, debutincubation, finincubation, 2, 1000, function (result, buckets) {
+            const userIds = buckets.users.buckets.map(function (bucket) {
+                return bucket.key
+            })
+
+            console.log(userIds);
+            res.status(200).send(userIds);
+        })
+        return;
 
         await Incubation.create({
             id: 0,
