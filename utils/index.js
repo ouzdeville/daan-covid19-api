842c4fa7f95f5d62ccccb269f26bfd8304aa3e95
ouzdeville <ouzdeville@gmail.com>
2020-06-27 18:52:30 +0000

    delete ble repo

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,5 @@ module.exports = {
   elasticClient: require('./elk-util'),
   prevalenceCron: require('./prevalenceCron'),
   cryptoUtil: require('./crypto/crypto-util'),
-  //bleUtil: require('./ble/crypto/bleCryptoTools'),
 
 }
